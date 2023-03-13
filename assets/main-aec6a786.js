bcc21047085a63c966c88247cd3c87609696ebd2
daw2-sorroche23 <sorrocheperezzeus@fpllefia.com>
2023-03-13 17:25:45 +0100

    Updates

--- assets/main-aec6a786.js
+++ assets/main-aec6a786.js
@@ -108,15 +108,22 @@ const cervezas = [
 ];
 const formulario = {
   template: `<h1>Selecciona tu cerveza y haz tu pedido</h1>
-<form class="m-3">
+<form class="m-3" id="validar">
     <div class="form-group">
-      <label class="text-white fs-3" for="validation01">Nombre</label>
-      <input type="text" class="form-control bg-black text-white" id="nombre" 
-             placeholder="Nombre" required>
+      <label class="text-white fs-3"    >Nombre de grupo</label>
+      <input type="text" class="form-control bg-black text-white" id="nombre" required maxlength="10" minlength="4"placeholder="Nombre" required>
+        <!-- mensaje si valida -->
+        <div class="valid-feedback">Todo correcto </div>
+        <!-- mensaje si no valida -->
+        <div class="invalid-feedback">Minimo de caracteres 4 y maximo 10</div>
     </div>
     <div class="form-group">
     <label class="text-white fs-3" for="validation02">Mesa</label>
-      <input type="number" class="form-control bg-black text-white" id="Mesa" placeholder="Numero de la mesa" required>
+      <input type="number" class="form-control bg-black text-white" id="Mesa" placeholder="Numero de la mesa"  min="1" max="15" required>
+         <!-- mensaje si valida -->
+         <div class="valid-feedback">Todo correcto </div>
+         <!-- mensaje si no valida -->
+        <div class="invalid-feedback">El nuemro de la mesa tiene que ser entre 1 y 15</div>
     </div>
     <div class="form-group">
         <label class="text-white fs-3" for="validation02">Cerveza</label>
@@ -124,10 +131,14 @@ const formulario = {
     </div>
     <div class="form-group">
         <label class="text-white fs-3" for="validation02">Cantidad</label>
-        <input type="text" class="form-control bg-black text-white" id="cantidad" placeholder="Cantidad de cervezas" required>
+        <input type="number" class="form-control bg-black text-white" id="cantidad" placeholder="Cantidad de cervezas" min="1" required>
+        <!-- mensaje si valida -->
+        <div class="valid-feedback">Todo correcto </div>
+        <!-- mensaje si no valida -->
+       <div class="invalid-feedback">No puedes pedir 0 consumiciones</div>
     </div>
     <div>
-        <button type="submit" class=" w-100 btn btn-primary">Submit</button>
+        <button type="submit" class=" w-100 btn btn-primary" id="enviar">Submit</button>
     </div> 
 </form>`,
   script: () => {
@@ -140,6 +151,16 @@ const formulario = {
     select.addEventListener("change", (event) => {
       descripcionCerveza.script(event.target.value);
     });
+    document.querySelector("#validar").classList.add("was-validated");
+    document.querySelector("#enviar").addEventListener("click", (e) => {
+      e.preventDefault();
+      const formEditar = document.querySelector("#validar");
+      formEditar.classList.add("was-validated");
+      if (formEditar.checkValidity()) {
+        formEditar.classList.remove("was-validated");
+        router.editarEnviar();
+      }
+    });
   }
 };
 const descripcionCerveza = {
@@ -147,9 +168,10 @@ const descripcionCerveza = {
   script: (id) => {
     const posicionCerveza = cervezas.findIndex((nombre) => nombre.id == id);
     const html = `<div>
+    // Seleccionamos del array un elemento y llamamos a las const posiconCerveza que tendra id de la cerveza que tengamos para posicionarnos
     <h1 class="text-white text-center">${cervezas[posicionCerveza].nombre}</h1>
-    <img class="w-25 m-3" src="${cervezas[posicionCerveza].imagen}" alt="FotoCerveza">
     <p class="text-bold text-white fw-bold fs-4">${cervezas[posicionCerveza].descripcion}</p>
+    <img class="w-25 m-3" src="${cervezas[posicionCerveza].imagen}" alt="FotoCerveza">
 </div>
 `;
     const descripcionCerveza2 = document.querySelector(".descripcion");
@@ -166,20 +188,7 @@ const tablaPedidos = {
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>Estrella Galicia</td>
-      <td>5</td>
-      <td><button class="btn bg-danger">Eliminar</button></td>
-      <td><button class="btn bg-primary">Editar</button></td> 
-    </tr>
-    <tr>
-      <td>San Miguel Especial</td>
-      <td>2</td>
-      <td><button class="btn bg-danger">Eliminar</button></td>
-      <td><button class="btn bg-primary">Editar</button></td> 
-    </tr>
-  </tbody>
-</table>`
+  `
 };
 document.querySelector("header").innerHTML = header.template;
 document.querySelector("main").innerHTML = home.template;
